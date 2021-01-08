6b15c8c7ee606129f3b680fd4d11a164edf87c68
Joey Sisk <jjmsisk@gmail.com>
2021-01-07 22:05:34 -0600

    updates sql to basic functionality, added basic switch statement to direct user to proper functions

--- app.js
+++ app.js
@@ -1,55 +1,76 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
-const [
-  VIEW_EMPLOYEES,
-  VIEW_ROLES,
-  VIEW_DEPARTMENTS,
-  ADD_EMPLOYEE,
-  REMOVE_EMPLOYEE,
-] = require("./lib/const");
+const VIEW_EMPLOYEES = "View all employees";
+const VIEW_ROLES = "View all role's";
+const VIEW_DEPARTMENTS = "View all department's"
+const ADD_EMPLOYEE = "Add a new employee";
+const REMOVE_EMPLOYEE = "Remove an existing employee";
 
-// create the connection information for the sql database
 const connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "T0mW@it$",
-  database: "employee_db",
+  database: "employee_manager_db",
 });
 
-// connect to the mysql server and sql database
-connection.connect(function (err) {
+
+connection.connect(async (err) => { // connect to the mysql server and sql database
   if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
+  console.log("connected as id " + connection.threadId);
+  await startApp();
+  connection.end();
 });
 
 // function which prompts the user for what action they should take
-async function start() {
-  try {
-    answer = await inquirer.prompt({
-      name: "main",
-      type: "list",
-      message: "What would you lke to do?",
-      choices: [
-        VIEW_EMPLOYEES,
-        VIEW_ROLES,
-        VIEW_DEPARTMENTS,
-        ADD_EMPLOYEE,
-        REMOVE_EMPLOYEE,
-        "EXIT",
-      ],
-    });
-  } catch (error) {
-    consol.log(error);
+async function startApp() {
+
+  let answer, query;
+
+  answer = await promptChoices();
+
+  switch(answer.name) {
+    case VIEW_DEPARTMENTS:
+      query = await promptViewDepartment();
+      viewADepartment();
+      break;
+    case VIEW_ROLES:
+      query = await promptViewRoles();
+      viewARoles();
+      break;
+    case VIEW_EMPLOYEES:
+      query = await promptViewEmployees();
+      viewEmployees();
+      break;
+    case ADD_EMPLOYEE:
+      addNewEmployee();
+      break;
+    case REMOVE_EMPLOYEE:
+      removeAnEmployee();
+      break;
   }
-}
+
+
+
+
+//   try {
+//     answer = await inquirer.prompt({
+//       name: "main",
+//       type: "list",
+//       message: "What would you lke to do?",
+//       choices: [
+//         VIEW_EMPLOYEES,
+//         VIEW_ROLES,
+//         VIEW_DEPARTMENTS,
+//         ADD_EMPLOYEE,
+//         REMOVE_EMPLOYEE,
+//         "EXIT",
+//       ],
+//     });
+//   } catch (error) {
+//     consol.log(error);
+//   }
+// }
 
 
