ab869556985763c07c396c9db35b0d8796116bee
Joey Sisk <jjmsisk@gmail.com>
2021-01-08 21:31:35 -0600

    added more inquirer questions for add an employee and functions into queries.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql");
-const inquirer = require("inquirer");
 
-const VIEW_EMPLOYEES = "View all employees";
-const VIEW_ROLES = "View all role's";
-const VIEW_DEPARTMENTS = "View all department's"
-const ADD_EMPLOYEE = "Add a new employee";
-const REMOVE_EMPLOYEE = "Remove an existing employee";
+const [
+  VIEW_EMPLOYEES,
+  VIEW_ROLES,
+  VIEW_DEPARTMENTS,
+  ADD_EMPLOYEE,
+  REMOVE_EMPLOYEE,
+] = require("./lib/const");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -15,8 +16,8 @@ const connection = mysql.createConnection({
   database: "employee_manager_db",
 });
 
-
-connection.connect(async (err) => { // connect to the mysql server and sql database
+connection.connect(async (err) => {
+  // connect to the mysql server and sql database
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   await startApp();
@@ -25,52 +26,34 @@ connection.connect(async (err) => { // connect to the mysql server and sql datab
 
 // function which prompts the user for what action they should take
 async function startApp() {
-
-  let answer, query;
+  let answer, newEmp, remEmp;
 
   answer = await promptChoices();
 
-  switch(answer.name) {
+  switch (answer.name) {
     case VIEW_DEPARTMENTS:
-      query = await promptViewDepartment();
       viewADepartment();
       break;
     case VIEW_ROLES:
-      query = await promptViewRoles();
       viewARoles();
       break;
     case VIEW_EMPLOYEES:
-      query = await promptViewEmployees();
       viewEmployees();
       break;
     case ADD_EMPLOYEE:
-      addNewEmployee();
+      newEmp = await promptAddEmployee();
+      addNewEmployee(
+        connection,
+        newEmp.firstName,
+        newEmp.lastName,
+        newEmp.roleId,
+        newEmp.managerId,
+        newEmp.isManager
+      );
       break;
     case REMOVE_EMPLOYEE:
+      remEmp = await promptRemoveEmployee();
       removeAnEmployee();
       break;
   }
-
-
-
-
-//   try {
-//     answer = await inquirer.prompt({
-//       name: "main",
-//       type: "list",
-//       message: "What would you lke to do?",
-//       choices: [
-//         VIEW_EMPLOYEES,
-//         VIEW_ROLES,
-//         VIEW_DEPARTMENTS,
-//         ADD_EMPLOYEE,
-//         REMOVE_EMPLOYEE,
-//         "EXIT",
-//       ],
-//     });
-//   } catch (error) {
-//     consol.log(error);
-//   }
-// }
-
-
+}
--- db.sql
+++ db.sql
@@ -1,60 +1,85 @@
-DROP DATABASE employee_manager_db;
+DROP DATABASE IF EXISTS employee_manager_db;
 
-CREATE TABLE employee_manager_db;
+CREATE DATABASE employee_manager_db;
 USE employee_manager_db;
 
 CREATE TABLE employee_db (
-	id INT NOT NULL;
-    first_name VARCHAR(30);
-    last_name VARCHAR(30);
-    role_id INT NULL;
-    manager_id INT NOT NULL;
-    is_manager BOOLEAN DEFAULT FALSE,
-    PRIMARY KEY (position)
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT NULL,
+manager_id INT,
+is_manager BOOLEAN DEFAULT FALSE,
+PRIMARY KEY (id)
 );
 
-CREATE TABLE role_db (
-	id INT PRIMARY KEY;
-    title VARCHAR(30);
-    salary DECIMAL(10, 4) NOT NULL;
-    department_id INT NOT NULL;
+CREATE TABLE roles_db (
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(30),
+salary DECIMAL(10, 2) NOT NULL,
+department_id INT NOT NULL,
+PRIMARY KEY (id)
 );
 
 CREATE TABLE department_db (
-	id INT NOT NULL;
-    name VARCHAR(30);
+id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(30),
+PRIMARY KEY (id)
 );
 
-SELECT * FROM employee_manager_db
+INSERT INTO department_db (name) 
+VALUES
+("Sales"),
+("HR"),
+("Accounting"),
+("IT"),
+("Legal");
 
---------------------------------------
+INSERT INTO roles_db (title, salary, department_id)
+VALUES
+("Sales Manager", 140000.00, 1),
+("HR Manager", 90000.00, 2),
+("Accounting Manager", 130000.00, 3),
+("IT Manager", 80000.00, 4),
+("Salesperson", 60000.00, 1),
+("HR Consultant", 50000.00, 2),
+("Accountant", 70000.00, 3),
+("IT Professional", 50000.00, 4),
+("Attourney", 200000.00, 5);
 
--- TRUNCATE TABLE role_db; ~~to clear out info~~
-
-INSERT INTO department_db (name, id) VALUES ("sales", 100);
-INSERT INTO department_db (name, id) VALUES ("hr", 200);
-INSERT INTO department_db (name, id) VALUES ("accounting", 300);
-INSERT INTO department_db (name, id) VALUES ("it", 400);
-INSERT INTO department_db (name, id) VALUES ("legal", 500)
+INSERT INTO employee_db (first_name, last_name, role_id, manager_id, is_manager)
+VALUES
+("Piper", "Sisk", 100, null, true),
+("Spencer", "Carlson", 200, null, true),
+("Oscar", "Charland", 300, null, true),
+("Jefferson", "Ellinger", 110, 1, false),
+("Tyler", "Mappes", 210, 2, false),
+("Alex", "Windhelm", 310, 3, false),
+("Winry", "Ember", 410, 4, false);
 
 -- add departments, roles, employee's
 INSERT INTO employee_db (first_name, last_name, role_id, manager_id ,is_manager) VALUES (?, ?, ?, ?, ?);
 
 -- view departments, roles, employee's
 SELECT *
-FROM department_db
+FROM department_db;
 
 SELECT *
-FROM role_db
+FROM roles_db;
 
 SELECT *
-FROM employee_db
+FROM employee_db;
 
 -- update roles
-SELECT title, first_name, last_name
-FROM role_db
-LEFT JOIN employee_db
-ON 
+
+UPDATE employee_db
+SET first_name = ?, last_name = ?, role_id = ?, manager_id = ?, is_manager = ?
+where id = ?;
+
+-- SELECT title, first_name, last_name
+-- FROM role_db
+-- LEFT JOIN employee_db
+-- ON 
 
 -- bonus: update employee managers
 
