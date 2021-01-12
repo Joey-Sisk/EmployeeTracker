8d2475751cc3636f6499a4d0b102c575a88951f6
Joey Sisk <jjmsisk@gmail.com>
2021-01-11 23:27:04 -0600

    commented code and small clean up and organization

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const mysql = require("mysql2");
 
-const [
+const [ // imports questions which will be used for inquirer and the switch statement
   VIEW_EMPLOYEES,
   VIEW_ROLES,
   VIEW_DEPARTMENTS,
   ADD_EMPLOYEE,
   ADD_DEPARTMENT,
   ADD_ROLE,
+  UPDATE_EMPLOYEE_ROLE,
   REMOVE_EMPLOYEE,
   REMOVE_DEPARTMENT,
   REMOVE_ROLE,
 ] = require("./lib/const");
 
-const {
+const { //all inquirer prompts
   promptChoices,
   promptAddEmployee,
+  promptUpdateEmployee,
   promptAddDepartment,
   promptAddRole,
   promptRemoveEmployee,
@@ -22,13 +24,14 @@ const {
   promptRemoveRole,
 } = require("./lib/prompts");
 
-const {
+const { // all database queries. If I had more time I would reduce these and make them more flexable using CRUD and ORM
   addNewEmployee,
   addNewDepartment,
   addNewRole,
   viewEmployees,
   viewADepartment,
   viewARoles,
+  updateAnEmployee,
   removeAnEmployee,
   findEmployees,
   getRoles,
@@ -38,7 +41,7 @@ const {
   removeARole,
 } = require("./lib/queries");
 
-const connection = mysql.createConnection({
+const connection = mysql.createConnection({ // connect to the database
   host: "localhost",
   port: 3306,
   user: "root",
@@ -47,7 +50,7 @@ const connection = mysql.createConnection({
 });
 
 connection.connect(async (err) => {
-  // connect to the mysql server and sql database
+  // connect to the mysql server and sql database and start the app
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   await startApp();
@@ -55,7 +58,7 @@ connection.connect(async (err) => {
 
 // function which prompts the user for what action they should take
 async function startApp() {
-  let answer, // given more time i would reduce this clutter and put more of these in functions
+  let answer, // given more time I would reduce this clutter and put more of these in functions
     roles,
     roleTitles,
     managers,
@@ -78,24 +81,29 @@ async function startApp() {
     whichRoleId,
     roleRows,
     roleNames,
-    remRole;
+    remRole,
+    updateEmployee,
+    employeeNames;
 
-  answer = await promptChoices();
+  answer = await promptChoices(); // "home page" main inquirer questions
 
   switch (answer.name) {
-    case VIEW_EMPLOYEES:
+    case VIEW_EMPLOYEES: // simple functiont use console.table()
       await viewEmployees(connection);
       startApp();
       break;
+
     case VIEW_DEPARTMENTS:
       await viewADepartment(connection);
       startApp();
       break;
+
     case VIEW_ROLES:
       await viewARoles(connection);
       startApp();
       break;
-    case ADD_EMPLOYEE:
+
+    case ADD_EMPLOYEE: // add employee by giving options taken from the sql db
       roles = await getRoles(connection);
       roleTitles = roles.map((role) => role.title); // Use map to create an array of only the role titles
 
@@ -112,7 +120,7 @@ async function startApp() {
       );
       isManagerFlag = newEmp.isManager === "YES";
 
-      await addNewEmployee(
+      await addNewEmployee( 
         connection,
         newEmp.firstName,
         newEmp.lastName,
@@ -120,14 +128,15 @@ async function startApp() {
         (newEmployeeMgr && newEmployeeMgr.id) || 0, // let's default to 0, if manager ID was not found
         isManagerFlag
       );
-      startApp();
+      startApp(); // by running this after every case you will always return to main menu
       break;
+
     case ADD_DEPARTMENT:
-      newDep = await promptAddDepartment();
-      console.log(newDep);
+      newDep = await promptAddDepartment(); // prompts that dont require a list from the sql are kept simple
       await addNewDepartment(connection, newDep.name);
       startApp();
       break;
+
     case ADD_ROLE:
       deps = await getDepartments(connection);
       depNames = deps.map((dep) => dep.name);
@@ -135,9 +144,7 @@ async function startApp() {
       newRoleDepartment = deps.find(
         (dep) => dep.name === newRole.department_name
       );
-
       salaryInt = parseInt(newRole.salary);
-
       await addNewRole(
         connection,
         newRole.roleName,
@@ -146,6 +153,27 @@ async function startApp() {
       );
       startApp();
       break;
+
+    case UPDATE_EMPLOYEE_ROLE:
+      employees_row = await findEmployees(connection);
+      employeeNames = employees_row.map(
+        (employee) => `${employee.first_name} ${employee.last_name}`
+      );
+      roleRows = await getRoles(connection);
+      roleNames = roleRows.map((role) => role.title);
+      employeeAndRole = await promptUpdateEmployee(employeeNames, roleNames);
+      updateEmployee = employees_row.find(
+        (employee) =>
+          `${employee.first_name} ${employee.last_name}` ===
+          employeeAndRole.which_employee
+      );
+      whichRoleId = roleRows.find(
+        (role) => role.title === employeeAndRole.role_title
+      );
+      await updateAnEmployee(connection, whichRoleId.id, updateEmployee.id);
+      startApp();
+      break;
+
     case REMOVE_EMPLOYEE:
       employees_row = await findEmployees(connection);
       choiceArray = employees_row.map(
@@ -158,6 +186,8 @@ async function startApp() {
       );
       await removeAnEmployee(connection, removeEmployee.id);
       startApp();
+      break;
+
     case REMOVE_DEPARTMENT:
       deps = await getDepartments(connection);
       depNames = deps.map((dep) => dep.name);
@@ -166,6 +196,7 @@ async function startApp() {
       await removeADepartment(connection, whichRoleId.id);
       startApp();
       break;
+
     case REMOVE_ROLE:
       roleRows = await getRoles(connection);
       roleNames = roleRows.map((role) => role.title);
@@ -175,7 +206,8 @@ async function startApp() {
       await removeARole(connection, whichRoleId.id);
       startApp();
       break;
+
     default:
-      connection.end();
+      connection.end(); // the user can close the server by choosing EXIT
   }
 }
